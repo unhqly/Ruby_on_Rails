4d43590a3e6fe7c90b79fd2d5267ce7583b1bb71
Unholy <ya.unholy1@mail.ru>
2023-05-22 22:59:23 +0300

    lesson 1 is done

--- README.md
+++ README.md
@@ -22,3 +22,10 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+ENTITIES: 
+* Tests
+* Questions
+* Answers
+* Teachers/Admins
+* Users
---
+++ config/initializers/version.rb
@@ -0,0 +1,14 @@
+module Eth
+
+  #Defines the major version of the {Eth} module
+  MAJOR = 1.freeze
+
+  #Defines the minor version of the {Eth} module
+  MINOR = 0.freeze
+
+  #Defines the patch version of the {Eth} module
+  PATCH = 0.freeze
+
+  #Defines the version string of the {Eth} module
+  VERSION = [MAJOR, MINOR, PATCH].join(".").freeze
+end
